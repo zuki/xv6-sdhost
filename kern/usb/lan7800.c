b0e392795fd48526edbd4c6114f22979a9df2982
Keiji Suzuki <zuki.ebetsu@gmail.com>
2026-01-12 15:42:54 +0900

    引数の誤りを修正

--- kern/usb/lan7800.c
+++ kern/usb/lan7800.c
@@ -38,7 +38,10 @@ void lan7800(lan7800_t *self, usb_function_t *func)
     self->usb_func.configure = lan7800_configure;
     self->bulk_in = 0;
     self->bulk_out = 0;
-    self->tx_buffer = (uint8_t *)kalloc(FRAME_BUFFER_SIZE+TX_HEADER_SIZE);
+    //self->tx_buffer = (uint8_t *)kalloc(FRAME_BUFFER_SIZE+TX_HEADER_SIZE);
+    // 4096アラインのためkmallocではなくkallocを使う(4096バイトでOK)
+    self->tx_buffer = (uint8_t *)kalloc();
+
 }
 
 void _lan7800(lan7800_t *self)
