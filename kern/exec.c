824a5d558a65ecf6d257b50038a4724d4dcc62cf
Keiji Suzuki <zuki.ebetsu@gmail.com>
2026-01-23 10:35:14 +0900

    dynamic bin化を実施。staticなbinが正常動作

--- kern/exec.c
+++ kern/exec.c
@@ -1,6 +1,7 @@
 #include <types.h>
 #include <linux/elf.h>
 #include <linux/auxvec.h>
+#include <exec.h>
 #include <trap.h>
 
 #include <string.h>
@@ -8,12 +9,14 @@
 #include <vm.h>
 #include <proc.h>
 #include <mm.h>
+#include <mmap.h>
 #include <memlayout.h>
 #include <syscall.h>
 #include <vfs.h>
 #include <filedesc.h>
 #include <cachepage.h>
 #include <linux/errno.h>
+#include <linux/mman.h>
 
 static uint64_t auxv[][2] = { { AT_PAGESZ, PGSIZE } };
 
@@ -69,19 +72,180 @@ static int get_file(char *path, struct proc *p, struct vfile **file)
     return 0;
 }
 
-#if 0
-static uint64_t load_interpreter(char *path, uint64_t *base)
+// bssからページ境界までゼロクリアする
+// |----bss_0000000000000000|
+static void padzero(uint64_t bss)
 {
-
-
+    uint64_t nbytes = ELF_PAGEOFFSET(bss);
+    if (nbytes) {
+        nbytes = ELF_MIN_ALIGN - nbytes;
+        memset((void *)bss, 0, nbytes);
+    }
 }
-#endif
+
+// bss領域用のマッピングを必要であれば作成する
+static void *set_brk(uint64_t start, uint64_t end)
+{
+    uint64_t old_start = start;
+    uint64_t old_end = end;
+
+    start = ELF_PAGEALIGN(start);
+    end = ELF_PAGEALIGN(end);
+    // 既存のマッピング（データ用）で間に合う場合は何もしない（既存のマッピングは0詰め済み）
+    if (end <= start) {
+        padzero(old_start);
+        return (void *)old_start;
+    }
+
+    trace("old_start=0x%llx, start=0x%llx, old_end=0x%llx, end=0x%llx",
+        old_start, start, old_end, end);
+
+    return mmap((void *)start, end - start, PROT_READ | PROT_WRITE,
+        MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE, NULL, 0);
+}
+
+static uint64_t load_interpreter(char *path, uint64_t *base, struct proc *p)
+{
+    struct vfile *file;                     // interpreterファイル
+    Elf64_Ehdr elf;                         // プログラムヘッダ作業用
+    Elf64_Phdr ph;                          // プログラムヘッダー格納用
+    uint64_t load_addr = 0;                 // interpreterロードアドレス
+    uint64_t bss_start = 0, bss_end = 0;    // bssの開始/終了アドレス
+    void *mapped;
+    size_t size;
+    boolean set = false;
+    long err = -ENOEXEC;
+    int i;
+
+    if ((err = get_file(path, p, &file)) < 0) {
+        error("get_file: %s, err: %d", path, err);
+        return err;
+    }
+
+    // elfヘッダーを読み込み、妥当性をチェックする
+    if (copy_cachepages(file, (char *)&elf, sizeof(elf), 0) < 0) {
+        trace("readelf bad");
+        goto bad;
+    }
+
+    if (ELF_CHECK_E_IDNET(&elf) == 0) {
+        trace("elf header magic invalid");
+        goto bad;
+    }
+    if (elf.e_ident[EI_CLASS] != ELFCLASS64) {
+        trace("64 bit program not supported");
+        goto bad;
+    }
+    if (elf.e_type != ET_DYN && elf.e_type != ET_EXEC) {
+        error("bad header type %d", elf.e_type);
+        goto bad;
+    }
+    if (elf.e_phentsize != sizeof(Elf64_Phdr)) {
+        error("e_phentsize is invalid: %d", elf.e_phentsize);
+        goto bad;
+    }
+
+    if (elf.e_phnum > ELF_MIN_ALIGN / sizeof(Elf64_Phdr)) {
+        error("e_phnum is invalide: %d", elf.e_phnum);
+        goto bad;
+    }
+    if (ph.p_memsz < ph.p_filesz) {
+        error("memsz smaller than filesz");
+        goto bad;
+    }
+    if (ph.p_vaddr + ph.p_memsz < ph.p_vaddr) {
+        trace("vaddr + memsz overflow");
+        goto bad;
+    }
+    trace("elf header check ok");
+
+    for (i = 0; i < elf.e_phnum; i++) {
+        int flags = 0;
+        int prot  = 0;
+        uint64_t vaddr = 0;
+        uint64_t k;
+
+        // プログラムヘッダーを1つ読み込む
+        if (copy_cachepages(file, (char *)&ph, sizeof(ph), elf.e_phoff + i * elf.e_phentsize)) {
+            error("read bad");
+            goto bad;
+        }
+
+        // PT_LOAD以外のプログラムヘッダーは無視する
+        if (ph.p_type != PT_LOAD) {
+            // trace("unsupported type 0x%x, skipped\n", ph.p_type);
+            continue;
+        }
+
+        // interpreterをmappingする
+        if (ph.p_flags & PF_R) prot |= PROT_READ;
+        if (ph.p_flags & PF_W) prot |= PROT_WRITE;
+        if (ph.p_flags & PF_X) prot |= PROT_EXEC;
+
+        /* テキスト領域はMAP_SHARED| MAP_DENYWRITE、データ領域はMAP_PRIVATE */
+        if (prot & PROT_WRITE)
+            flags = MAP_PRIVATE;
+        else
+            flags = MAP_SHARED | MAP_DENYWRITE;
+
+        vaddr = ph.p_vaddr;
+
+        // ET_EXECまたは2番目以降のプログラムヘッダはアドレス固定
+        if (elf.e_type == ET_EXEC || set)
+            flags |= MAP_FIXED;
+        // それ以外は固定ではないが、初期値を与える
+        else
+            /* load_addrの初期値をELF_ET_DYN_BASEの1ページ下に設定する */
+            load_addr = ELF_PAGESTART(ELF_ET_DYN_BASE - vaddr);
+
+        // データをmappingする
+        mapped = mmap((void *)ELF_PAGESTART(load_addr + vaddr),
+                      ph.p_filesz + ELF_PAGEOFFSET(vaddr),
+                      prot, flags, file, ph.p_offset - ELF_PAGEOFFSET(vaddr));
+        if (IS_ERR(mapped)) {
+            err = (long)mapped;
+            goto bad;
+        }
+
+        // 1番目のPT_LOAD固有の処理
+        if (!set && elf.e_type == ET_DYN) {
+            load_addr = (uint64_t)mapped - ELF_PAGESTART(vaddr);
+            set = true;
+        }
+
+        // bss領域の設定
+        k = load_addr + vaddr + ph.p_filesz;
+        if (k > bss_start) bss_start = k;
+        k = load_addr + vaddr + ph.p_memsz;
+        if (k > bss_end) bss_end = k;
+    }
+
+    padzero(bss_start);
+    bss_start = ELF_PAGESTART(bss_start + ELF_MIN_ALIGN - 1);
+    if (bss_end > bss_start) {
+        mapped = set_brk(bss_start, bss_end);
+        if (IS_ERR(mapped)) {
+            err = (long)mapped;
+            goto bad;
+        }
+    }
+
+    *base = load_addr;
+    err = ((uint64_t)elf.e_entry + load_addr);
+
+bad:
+    vfs_close(file);
+    return err;
+}
 
 int execve(const char *path, char *const argv[], char *const envp[])
 {
     struct vfile *file;
-    char *s;
-    int err;
+    char *s, *ip;
+    int has_ip = 0;
+    uint64_t ip_entry;
+    uint64_t ip_base;
+    long err = -EACCES;
 
     trace("path='%s', argv=%p, envp=%p", path, argv, envp);
 
@@ -92,25 +256,6 @@ int execve(const char *path, char *const argv[], char *const envp[])
         error("get_file: %s, err: %d", path, err);
         return err;
     }
-
-#if 0
-    // 指定されたファイルが実行可能であるか確認する
-    if (vfs_access(curproc->cwd, path, X_OK, curproc->uid, 0) < 0) {
-        error("uid %d can't access %s", curproc->uid, path);
-        return -EPERM;
-    }
-
-    // 指定されたファイルをopen
-    if ((err = vfs_open(curproc->cwd, path, O_RDONLY, 0, curproc->uid, &file)) < 0) {
-        error("failed open %s", path);
-        return err;
-    }
-    trace("open ok: f->vnode->ino: %d", file->vnode->ino);
-    if (!S_ISREG(file->vnode->mode)) {
-        vfs_close(file);
-        return -EISDIR;
-    }
-#endif if
 
     // 呼び出し元のページテーブルをoldpgdirに保存し、
     // 実行ファイル用のページテーブルをpgdirとして新規作成する
@@ -128,19 +273,17 @@ int execve(const char *path, char *const argv[], char *const envp[])
         goto bad;
     }
 
-    if (!(  elf.e_ident[EI_MAG0] == ELFMAG0
-         && elf.e_ident[EI_MAG1] == ELFMAG1
-         && elf.e_ident[EI_MAG2] == ELFMAG2
-         && elf.e_ident[EI_MAG3] == ELFMAG3)) {
+    if (ELF_CHECK_E_IDNET(&elf) == 0) {
         trace("elf header magic invalid");
         goto bad;
     }
+
     if (elf.e_ident[EI_CLASS] != ELFCLASS64) {
         trace("64 bit program not supported");
         goto bad;
     }
 
-    if (elf.e_type != ET_EXEC) {
+    if (elf.e_type != ET_EXEC && elf.e_type != ET_DYN) {
         error("bad header type %d", elf.e_type);
         goto bad;
     }
@@ -153,6 +296,7 @@ int execve(const char *path, char *const argv[], char *const envp[])
         curproc->fsuid = file->vnode->uid;
     else
         curproc->fsuid = curproc->uid;
+
     // ファイルのSet-gidをセットする: stickyビットの対応
     if (((file->vnode->mode & (S_ISGID | S_IXGRP)) == (S_ISGID | S_IXGRP)) && curproc->gid != 0)
         curproc->fsgid = file->vnode->gid;
@@ -168,7 +312,6 @@ int execve(const char *path, char *const argv[], char *const envp[])
     size_t sz = 0;      // 最後にuvm_allocしたプログラムヘッダのp_msizeを保持
     size_t base = 0;    // 最初のプロセスヘッダのp_vaddrを保持
     size_t stksz = 0;   // スタックサイズを保持
-    size_t offset;
     int first = 1;      // 最初のプログラムヘッダか? : 特別な処理が必要なため
 
     for (i = 0; i < elf.e_phnum; i++) {
@@ -176,6 +319,24 @@ int execve(const char *path, char *const argv[], char *const envp[])
         if (copy_cachepages(file, (char *)&ph, sizeof(ph), elf.e_phoff + i * elf.e_phentsize)) {
             error("read bad");
             goto bad;
+        }
+
+        if (ph.p_type == PT_INTERP) {
+            if (ph.p_filesz > PGSIZE) {
+                error("name of interpretor is too long: 0x%x", ph.p_filesz);
+                goto bad;
+            }
+            ip = (char *)kmalloc(ph.p_filesz);
+            if (!ip) {
+                error("could't get memory for interpretor name");
+                goto bad;
+            }
+            if ((err = copy_cachepages(file, ip, ph.p_filesz, ph.p_offset)) < 0) {
+                error("failed get interpretor name");
+                goto bad;
+            }
+            has_ip = 1;
+            continue;
         }
 
         // PT_LOAD以外のプログラムヘッダーは無視する
@@ -214,21 +375,6 @@ int execve(const char *path, char *const argv[], char *const envp[])
 
         uvm_switch(pgdir);
 
-#if 0
-        offset = file->offset;
-        if (vfs_seek(file, ph.p_offset, SEEK_SET) < 0) {
-            trace("failed seek");
-            goto bad;
-        }
-        if (vfs_read(file, (char *)ph.p_vaddr, ph.p_filesz) < 0) {
-            trace("read section bad");
-            goto bad;
-        }
-        if ((offset = vfs_seek(file, offset, SEEK_SET)) < 0) {
-            trace("failed seek");
-            goto bad;
-        }
-#endif
         if (copy_cachepages(file, (char *)ph.p_vaddr, ph.p_filesz, ph.p_offset) < 0) {
             error("ph[%d].p_files load error", i);
             goto bad;
@@ -247,25 +393,49 @@ int execve(const char *path, char *const argv[], char *const envp[])
               ph.p_vaddr + ph.p_memsz);
     }
 
+    // これ以前は、エラーがあった場合は goto bad
     vfs_close(file);
+    // 以後、エラーがあった場合は goto free_ip
     trace("load file ok");
+
+    // インタープリタが指定されていた場合
+    if (has_ip) {
+        ip_entry = load_interpreter(ip, &ip_base, curproc);
+        if (IS_ERR((void *)ip_entry)) {
+            error("failed load interpretor: %s", ip);
+            goto free_ip;
+        }
+    }
 
     // Push argument strings, prepare rest of stack in ustack.
     uvm_switch(oldpgdir);
     char *sp = (char *)USERTOP;
     int argc = 0, envc = 0;
     size_t len;
+
+    // platform名("aarch64")のセット
+    uint64_t platform = 0;
+    if (has_ip) {
+        len = strlen(ELF_PLATFORM) + 1;
+        sp -= len;
+        platform = (uint64_t)sp;
+        if (copyout(pgdir, sp, ELF_PLATFORM, len) < 0) {
+            error("failed copyout platform");
+            goto free_ip;
+        }
+    }
+
     if (argv) {
         for (; in_user((void *)(argv + argc), sizeof(*argv)) && argv[argc];
              argc++) {
             if ((len = fetchstr((uint64_t) argv[argc], &s)) < 0) {
                 trace("argv fetchstr bad");
-                goto bad;
+                goto free_ip;
             }
             trace("argv[%d] = '%s', len: %d", argc, argv[argc], len);
             sp -= len + 1;
             if (copyout(pgdir, sp, argv[argc], len + 1) < 0)    // include '\0';
-                goto bad;
+                goto free_ip;
         }
     }
     trace("copy argv ok");
@@ -274,21 +444,44 @@ int execve(const char *path, char *const argv[], char *const envp[])
              envc++) {
             if ((len = fetchstr((uint64_t) envp[envc], &s)) < 0) {
                 trace("envp fetchstr bad");
-                goto bad;
+                goto free_ip;
             }
             trace("envp[%d] = '%s', len: %d", envc, envp[envc], len);
             sp -= len + 1;
             if (copyout(pgdir, sp, envp[envc], len + 1) < 0)    // include '\0';
-                goto bad;
+                goto free_ip;
         }
     }
     trace("copy envp ok")
+    uint64_t auxv_size;
+    uint64_t auxv_dyn[][2] = {
+        { AT_PHDR,    elf.e_phoff },
+        { AT_PHENT,   sizeof(Elf64_Phdr) },
+        { AT_PHNUM,   elf.e_phnum },
+        { AT_PAGESZ,  PGSIZE },
+        { AT_BASE,    ip_base },                // インタプリタのbaseアドレス
+        { AT_FLAGS,   0 },
+        { AT_ENTRY,   elf.e_entry },            // プログラムのエントリポイント
+        { AT_UID,     (uint64_t) curproc->uid },
+        { AT_EUID,    (uint64_t) curproc->euid },
+        { AT_GID,     (uint64_t) curproc->gid },
+        { AT_EGID,    (uint64_t) curproc->egid },
+        { AT_PLATFORM, platform },
+        { AT_HWCAP,   ELF_HWCAP },
+        { AT_CLKTCK,  HZ },
+        { AT_NULL,    0 }
+
+    };
+    uint64_t auxv_sta[][2] = { { AT_PAGESZ, PGSIZE } };
+
+    auxv_size = has_ip ? sizeof(auxv_dyn) : sizeof(auxv_sta);
+
     // Align to 16B. 3 zero terminator of auxv/envp/argv and 1 argc.
     void *newsp =
-        (void *)ROUNDDOWN((size_t)sp - sizeof(auxv) -
+        (void *)ROUNDDOWN((size_t)sp - auxv_size -
                           (envc + argc + 4) * 8, 16);
     if (copyout(pgdir, newsp, 0, (size_t)sp - (size_t)newsp) < 0)
-        goto bad;
+        goto free_ip;
 
     uvm_switch(pgdir);
 
@@ -297,6 +490,11 @@ int execve(const char *path, char *const argv[], char *const envp[])
     uint64_t *newauxv = (void *)newenvp + 8 * (envc + 1);
     trace("argv: 0x%p, envp: 0x%p, auxv: 0x%p", newargv, newenvp, newauxv);
     memmove(newauxv, auxv, sizeof(auxv));
+
+    if (has_ip)
+        memmove(newauxv, auxv_dyn, auxv_size);
+    else
+        memmove(newauxv, auxv_sta, auxv_size);
 
     for (int i = envc - 1; i >= 0; i--) {
         newenvp[i] = (uint64_t) sp;
@@ -318,7 +516,7 @@ int execve(const char *path, char *const argv[], char *const envp[])
     if (copyout
         (pgdir, (void *)(USERTOP - stksz), 0,
          stksz - (USERTOP - (size_t)sp)) < 0)
-        goto bad;
+        goto free_ip;
 
     assert((uint64_t) sp > USERTOP - stksz);
     trace("proc: base=0x%x, size=0x%x, stack: sp=0x%x, size=0x%x", base, sz, sp, stksz);
@@ -330,12 +528,14 @@ int execve(const char *path, char *const argv[], char *const envp[])
     curproc->sz = sz;
     curproc->stksz = stksz;
 
-    // memset(curproc->tf, 0, sizeof(*curproc->tf));
+    if (has_ip)
+        curproc->tf->elr = ip_entry;
+    else
+        curproc->tf->elr = elf.e_entry;
 
-    curproc->tf->elr = elf.e_entry;
     curproc->tf->sp = (uint64_t) sp;
 
-    trace("entry 0x%p", elf.e_entry);
+    trace("entry 0x%p", curproc->tf->elr);
 
     uvm_switch(oldpgdir);
 
@@ -350,9 +550,15 @@ int execve(const char *path, char *const argv[], char *const envp[])
     uvm_switch(curproc->pgdir);
     vm_free(oldpgdir);
     trace("exec %s ok", curproc->name);
+    if (has_ip && ip)
+        kmfree((void *)ip);
+
     return 0;
 
-  bad:
+free_ip:
+    if (has_ip && ip)
+        kmfree(ip);
+bad:
     vfs_close(file);
     if (pgdir)
         vm_free(pgdir);
